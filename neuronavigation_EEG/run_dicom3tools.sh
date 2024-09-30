44285ac40b732d04a3f973d5f30b05a97e70068f
giangiannini <118919417+giangiannini@users.noreply.github.com>
2024-09-30 16:31:27 +0200

    Update run_dicom3tools.sh

--- neuronavigation_EEG/run_dicom3tools.sh
+++ neuronavigation_EEG/run_dicom3tools.sh
@@ -1,5 +1,11 @@
-INPUTFULE="/Volumes/DATI/ID11/CCNB_10673_vDMT.MR.CCNB_Projekte.12.1.2023.05.09.18.24.26.137.79166621.dcm"
-OUTPUTDIRECTORY="/Volumes/DATI/ID11/unenhanced/"
+#This script will run the function dcuncat in order to "unenhance" the 3D dicom images located in the INPUTFILE folder
+#into a slice series of 2D images located in the OUTPUTDIRECTORY folder. 
+#Because dcuncat do not automatically assign the ".dcm" extension to the exported files, this is done afterwards by the function. 
+#Before running, make sure that the variables match your system and that dicom3tools is installed. 
+#For any problem, do not hesitate to contact me: gianluigi.giannini@fu-berlin.de
+
+INPUTFULE="/Users/YOURUSERNAME/Desktop/imgs_inpu/enhanced.dcm"
+OUTPUTDIRECTORY="/Users/YOURUSERNAME/Desktop/imgs_output/"
 OUTPUTFILE="ID11"
 SEPARATOR="_"
 

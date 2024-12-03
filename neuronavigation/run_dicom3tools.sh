c509cd85ef60a1b367f99705962b31290ca6ea17
Miro Grundei <m.grundei@posteo.de>
2024-12-03 14:03:56 +0100

    Add files via upload

---
+++ neuronavigation/run_dicom3tools.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Adjusted for windows by Miro: Code needed to be adjusted to ensure Windows-style line endings (CRLF) 
+
+#This script will run the function dcuncat in order to "unenhance" the 3D dicom images located in the INPUTFILE folder
+#into a slice series of 2D images located in the OUTPUTDIRECTORY folder. 
+#Because dcuncat do not automatically assign the ".dcm" extension to the exported files, this is done afterwards by the function. 
+#Before running, make sure that the variables match your system and that dicom3tools is installed. 
+#For any problem, do not hesitate to contact me: gianluigi.giannini@fu-berlin.de
+
+SUB="2209"
+
+INPUTFULE="E:/MeMoSLAP_Phase2/neuronav/$SUB/1.dcm"
+OUTPUTDIRECTORY="E:/MeMoSLAP_Phase2/neuronav/$SUB/unenhanced_t1/sub-$SUB/"
+mkdir -p "$OUTPUTDIRECTORY"
+OUTPUTFILE="t1"
+SEPARATOR="_"
+
+cd $OUTPUTDIRECTORY 
+
+dcuncat -if "$INPUTFULE" -of "$OUTPUTDIRECTORY$OUTPUTFILE$SEPARATOR" -unenhance
+
+for file in *; do
+	mv "$file" "$file.dcm"
+done

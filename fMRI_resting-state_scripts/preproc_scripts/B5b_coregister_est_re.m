c61cf0eb7a0d90e4a34f811433bcb7604bf0ded3
Sreya Priyadarsini <sreya1204@gmail.com>
2026-07-02 00:26:48 +0200

    Update B5b_coregister_est_re.m

--- fMRI_resting-state_scripts/preproc_scripts/B5b_coregister_est_re.m
+++ fMRI_resting-state_scripts/preproc_scripts/B5b_coregister_est_re.m
@@ -6,19 +6,13 @@ f1 = spm_select('List', struct_dir, filter_struct);
 numVols = size(f1,1);
 structural = cellstr([repmat([struct_dir filesep], numVols, 1) f1 repmat(',1', numVols, 1)]);
 
-f2 = spm_select('List', func_dir, ['^mean' run]);
+f2 = spm_select('List', func_dir, ['^mean' runs{1}]); 
 numVols = size(f2,1);
 mean_img   = cellstr([repmat([func_dir filesep], numVols, 1) f2 repmat(',1', numVols, 1)]);
 
 for r = 1:size(runs,2)
-%     data_dir = [sj_dir filesep sbj_level_folder filesep 'mean_bin_' num2str(b)];
-%     f3 = spm_select('List', data_dir, '^res_accuracy_minus_chance.nii');
-%     numVols = size(f3,1);
-%     Images{b} = cellstr([repmat([data_dir filesep], numVols, 1) f3 repmat(',1', numVols, 1)]);
     f3 = spm_select('ExtFPList', func_dir, ['^' currPrefix runs{r}],Inf);
     numVols = size(f3,1);
-%     Images{r,:}=cellstr([repmat([func_dir filesep], numVols, 1) f3 repmat(',1', numVols, 1)]);
-%     Images{r,:}=cellstr([repmat([func_dir filesep], numVols, 1) f3]);
     Images{r,:}=cellstr(spm_select('ExtFPList', func_dir,['^' currPrefix runs{r}],Inf));
 end
 %--------------------------------------------------------------------------
